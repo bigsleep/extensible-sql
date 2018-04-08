4db1208e2fa4991412f115efd1d6437e12d0dbe6
tkaaad97 <tkaaad97@gmail.com>
2018-04-08 20:15:08 +0900

    handle bracket

--- app/Main.hs
+++ app/Main.hs
@@ -4,28 +4,31 @@ module Main where
 import Data.Extensible (Member, Match(..), (:*), (<:), nil)
 import ExSql.Syntax.Arithmetic
 import ExSql.Syntax.Comparison
+import ExSql.Syntax.Logical
 import ExSql.Syntax.Literal
 import ExSql.Syntax.Class
+import ExSql.Syntax.Relativity
 import ExSql.Printer.Default
 
-e1 :: (Member xs Comparison, Member xs Arithmetic, Member xs Literal) => Expr xs Bool
-e1 = equality (addition (multiplication (int 1) (negation (int 2))) (division (int 3) (int 4))) (multiplication (int 5) (int 2))
+e1 :: (Member xs Comparison, Member xs Arithmetic, Member xs Literal, Member xs Logical) => Expr xs Bool
+e1 = disjunction ((bool False) conjunction (equality (addition (multiplication (int 1) (negation (int 2))) (division (int 3) (addition (int 4) (int 2)))) (multiplication (int 5) (int 2))) (bool True))
 
-type Nodes = '[Comparison, Arithmetic, Literal]
+type Nodes = '[Logical, Comparison, Arithmetic, Literal]
 type E = Expr Nodes
 type Printers xs a = Printer (Expr xs) :* xs
 
-printers :: (forall b. Expr Nodes b -> String) -> Printers Nodes a
+printers :: (forall b. Maybe Relativity -> Maybe Relativity -> Expr Nodes b -> String) -> Printers Nodes a
 printers p
-    =  Printer (prettyComparison p)
+    =  Printer (prettyLogical p)
+    <: Printer (prettyComparison p)
     <: Printer (prettyArithmetic p)
     <: Printer (prettyLiteral)
     <: nil
 
-pp :: Expr Nodes a -> String
+pp :: Maybe Relativity -> Maybe Relativity -> Expr Nodes a -> String
 pp = pretty (printers pp)
 
 main :: IO ()
 main = do
-    putStrLn $ pp e1
+    putStrLn $ pp Nothing Nothing e1
     putStrLn "hello"
--- src/ExSql/Printer/Default.hs
+++ src/ExSql/Printer/Default.hs
@@ -12,35 +12,86 @@ import Data.Extensible ((:*), (:|)(..), hindex)
 import Data.Functor.Identity (Identity(..))
 import Data.Text (Text)
 import ExSql.Syntax.Class (Expr(..), Node(..))
+import ExSql.Syntax.Relativity (Relativity(..), Precedence(..), Associativity(..))
 import ExSql.Syntax.Literal
 import ExSql.Syntax.Arithmetic
 import ExSql.Syntax.Comparison
 import ExSql.Syntax.Logical
 
-newtype Printer (g :: * -> *) (v :: (* -> *) -> * -> *) = Printer { runPrinter :: forall a. v g a -> String }
+newtype Printer (g :: * -> *) (v :: (* -> *) -> * -> *) = Printer { runPrinter :: forall a. Maybe Relativity -> Maybe Relativity -> v g a -> String }
 
-pretty :: Printer (Expr xs) :* xs -> Expr xs a -> String
-pretty printers (Expr (EmbedAt membership (Node a))) = runPrinter (hindex printers membership) a
+pretty :: Printer (Expr xs) :* xs -> Maybe Relativity -> Maybe Relativity -> Expr xs a -> String
+pretty printers l r (Expr (EmbedAt membership (Node a))) = runPrinter (hindex printers membership) l r a
 
-prettyLiteral :: Literal g a -> String
-prettyLiteral (LitInt a) = show a
-prettyLiteral (LitBool a) = show a
+prettyLiteral :: Maybe Relativity -> Maybe Relativity -> Literal g a -> String
+prettyLiteral _ _ (LitInt a) = show a
+prettyLiteral _ _ (LitBool a) = show a
 
-prettyComparison :: (forall b. Expr xs b -> String) -> Comparison (Expr xs) a -> String
-prettyComparison p (Equality a0 a1) = "(" ++ p a0 ++ "==" ++ p a1 ++ ")"
-prettyComparison p (GreaterThan a0 a1) = "(" ++ p a0 ++ ">" ++ p a1 ++ ")"
-prettyComparison p (LessThan a0 a1) = "(" ++ p a0 ++ "<" ++ p a1 ++ ")"
-prettyComparison p (GreaterThanOrEqual a0 a1) = "(" ++ p a0 ++ ">=" ++ p a1 ++ ")"
-prettyComparison p (LessThanOrEqual a0 a1) = "(" ++ p a0 ++ "<=" ++ p a1 ++ ")"
+prettyComparison :: (forall b. Maybe Relativity -> Maybe Relativity -> Expr xs b -> String) -> Maybe Relativity -> Maybe Relativity -> Comparison (Expr xs) a -> String
+prettyComparison p l r (Equality a0 a1) =
+    let c = Relativity (Precedence 11) NonAssociative
+    in handleBracket l c r $ p l (Just c) a0 ++ "==" ++ p (Just c) r a1
+prettyComparison p l r (GreaterThan a0 a1) =
+    let c = Relativity (Precedence 11) NonAssociative
+    in handleBracket l c r $ p l (Just c) a0 ++ ">" ++ p (Just c) r a1
+prettyComparison p l r (LessThan a0 a1) =
+    let c = Relativity (Precedence 11) NonAssociative
+    in handleBracket l c r $ p l (Just c) a0 ++ "<" ++ p (Just c) r a1
+prettyComparison p l r (GreaterThanOrEqual a0 a1) =
+    let c = Relativity (Precedence 11) NonAssociative
+    in handleBracket l c r $ p l (Just c) a0 ++ ">=" ++ p (Just c) r a1
+prettyComparison p l r (LessThanOrEqual a0 a1) =
+    let c = Relativity (Precedence 11) NonAssociative
+    in handleBracket l c r $ p l (Just c) a0 ++ "<=" ++ p (Just c) r a1
 
-prettyArithmetic :: (forall b. Expr xs b -> String) -> Arithmetic (Expr xs) a -> String
-prettyArithmetic p (Negation a) = "(-" ++ p a ++ ")"
-prettyArithmetic p (Addition a0 a1) = "(" ++ p a0 ++ "+" ++ p a1 ++ ")"
-prettyArithmetic p (Subtraction a0 a1) = "(" ++ p a0 ++ "-" ++ p a1 ++ ")"
-prettyArithmetic p (Multiplication a0 a1) = "(" ++ p a0 ++ "*" ++ p a1 ++ ")"
-prettyArithmetic p (Division a0 a1) = "(" ++ p a0 ++ "/" ++ p a1 ++ ")"
+prettyArithmetic :: (forall b. Maybe Relativity -> Maybe Relativity -> Expr xs b -> String) -> Maybe Relativity -> Maybe Relativity -> Arithmetic (Expr xs) a -> String
+prettyArithmetic p l r (Negation a) =
+    let c = Relativity (Precedence 4) RightToLeft
+    in handleBracket l c r $ "-" ++ p (Just c) r a
+prettyArithmetic p l r (Addition a0 a1) =
+    let c = Relativity (Precedence 7) LeftToRight
+    in handleBracket l c r $ p l (Just c) a0 ++ "+" ++ p (Just c) r a1
+prettyArithmetic p l r (Subtraction a0 a1) =
+    let c = Relativity (Precedence 7) LeftToRight
+    in handleBracket l c r $ p l (Just c) a0 ++ "-" ++ p (Just c) r a1
+prettyArithmetic p l r (Multiplication a0 a1) =
+    let c = Relativity (Precedence 6) LeftToRight
+    in handleBracket l c r $ p l (Just c) a0 ++ "*" ++ p (Just c) r a1
+prettyArithmetic p l r (Division a0 a1) =
+    let c = Relativity (Precedence 6) LeftToRight
+    in handleBracket l c r $ p l (Just c) a0 ++ "/" ++ p (Just c) r a1
 
-prettyLogical :: (forall b. Expr xs b -> String) -> Logical (Expr xs) a -> String
-prettyLogical p (LogicalNegation a) = "(!" ++ p a ++ ")"
-prettyLogical p (Conjunction a0 a1) = "(" ++ p a0 ++ "&&" ++ p a1 ++ ")"
-prettyLogical p (Disjunction a0 a1) = "(" ++ p a0 ++ "||" ++ p a1 ++ ")"
+prettyLogical :: (forall b. Maybe Relativity -> Maybe Relativity -> Expr xs b -> String) -> Maybe Relativity -> Maybe Relativity -> Logical (Expr xs) a -> String
+prettyLogical p l r (LogicalNegation a) =
+    let c = Relativity (Precedence 13) RightToLeft
+    in handleBracket l c r $ "NOT " ++ p (Just c) r a
+prettyLogical p l r (Conjunction a0 a1) =
+    let c = Relativity (Precedence 14) LeftToRight
+    in handleBracket l c r $ p l (Just c) a0 ++ " AND " ++ p (Just c) r a1
+prettyLogical p l r (Disjunction a0 a1) =
+    let c = Relativity (Precedence 16) LeftToRight
+    in handleBracket l c r $ p l (Just c) a0 ++ " OR " ++ p (Just c) r a1
+
+handleBracket :: Maybe Relativity -> Relativity -> Maybe Relativity -> String -> String
+handleBracket l c r s = if needBracket l c r
+    then "(" ++ s ++ ")"
+    else s
+
+needBracket :: Maybe Relativity -> Relativity -> Maybe Relativity -> Bool
+needBracket l c r = needBracketL c r || needBracketR l c
+
+needBracketL :: Relativity -> Maybe Relativity -> Bool
+needBracketL (Relativity p a) (Just (Relativity rp ra))
+    | p > rp = True
+    | a == NonAssociative = True
+    | p == rp && a == LeftToRight = True
+    | otherwise = False
+needBracketL _ _ = False
+
+needBracketR :: Maybe Relativity -> Relativity -> Bool
+needBracketR (Just (Relativity lp la)) (Relativity p a)
+    | p > lp = True
+    | a == NonAssociative = True
+    | p == lp && a == RightToLeft = True
+    | otherwise = False
+needBracketR _ _ = False
