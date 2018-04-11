1772ff4fcac9b5c76ede6c3be3ac0d7f385e3e84
tkaaad97 <tkaaad97@gmail.com>
2018-04-12 02:06:03 +0900

    hositExpr

--- src/ExSql/Syntax/Class.hs
+++ src/ExSql/Syntax/Class.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, ConstraintKinds, TypeFamilies, RankNTypes, FlexibleContexts, TypeOperators #-}
+{-# LANGUAGE DataKinds, ConstraintKinds, TypeFamilies, RankNTypes, FlexibleContexts, TypeOperators, GADTs #-}
 module ExSql.Syntax.Class
     ( Ast(..)
     , Hoist(..)
@@ -10,17 +10,18 @@ module ExSql.Syntax.Class
     , binaryOp
     ) where
 
-import Data.Extensible (Member, (:|), embed)
+import Data.Extensible (Member, (:|)(..), embed)
 import GHC.TypeLits (Symbol)
 
 class Ast g where
     type NodeTypes g :: [(* -> *) -> * -> *]
-    mkAst :: (Member (NodeTypes g) v) => v g a -> g a
+    mkAst :: (Hoist v, Member (NodeTypes g) v) => v g a -> g a
 
 class Hoist (t :: (* -> *) -> * -> *) where
     hoist :: (forall x. f x -> g x) -> t f a -> t g a
 
-newtype Node m g a (f :: (* -> *) -> * -> *) = Node { unNode :: m (f g a) }
+data Node m g a (f :: (* -> *) -> * -> *) where
+    Node :: (Hoist f) => m (f g a) -> Node m g a f
 
 newtype Expr m xs a = Expr { unExpr :: Node m (Expr m xs) a :| xs }
 
@@ -28,12 +29,15 @@ instance (Monad m) => Ast (Expr m xs) where
     type NodeTypes (Expr m xs) = xs
     mkAst = Expr . embed . Node . return
 
-type UnaryOpType v g a b = (Ast g, Member (NodeTypes g) v) => g a -> g b
+hoistExpr :: (Monad n) => (forall x. m x -> n x) -> Expr m xs a -> Expr n xs a
+hoistExpr f (Expr (EmbedAt p (Node m))) = Expr (EmbedAt p (Node (hoist (hoistExpr f) <$> f m)))
 
-type BinaryOpType v g a b = (Ast g, Member (NodeTypes g) v) => g a -> g a -> g b
+type UnaryOpType v g a b = (Ast g, Hoist v, Member (NodeTypes g) v) => g a -> g b
 
-unaryOp :: (Ast g, Member (NodeTypes g) v) => (g a -> v g b) -> g a -> g b
+type BinaryOpType v g a b = (Ast g, Hoist v, Member (NodeTypes g) v) => g a -> g a -> g b
+
+unaryOp :: (Ast g, Hoist v, Member (NodeTypes g) v) => (g a -> v g b) -> g a -> g b
 unaryOp op = mkAst . op
 
-binaryOp :: (Ast g, Member (NodeTypes g) v) => (g a -> g a -> v g b) -> g a -> g a -> g b
+binaryOp :: (Ast g, Hoist v, Member (NodeTypes g) v) => (g a -> g a -> v g b) -> g a -> g a -> g b
 binaryOp op a0 a1 = mkAst $ a0 `op` a1
