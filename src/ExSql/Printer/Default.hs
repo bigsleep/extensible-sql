8ddf3fc82d67c9a49772c3027b4ad1be57ffe5e5
tkaaad97 <tkaaad97@gmail.com>
2018-05-10 23:41:37 +0900

    function

--- src/ExSql/Printer/Default.hs
+++ src/ExSql/Printer/Default.hs
@@ -14,15 +14,16 @@ import Data.Extensible ((:*), (:|)(..), hindex)
 import Data.Functor.Identity (Identity(..))
 import Data.Int (Int64)
 import Data.Text (Text)
-import qualified Data.Text as Text (pack)
+import qualified Data.Text as Text (pack, intercalate)
 
 import Database.Persist (PersistValue(..))
 
 import ExSql.Syntax.Class (Expr(..), Node(..))
 import ExSql.Syntax.Relativity (Relativity(..), Precedence(..), Associativity(..))
-import ExSql.Syntax.Literal
 import ExSql.Syntax.Arithmetic
 import ExSql.Syntax.Comparison
+import ExSql.Syntax.Function
+import ExSql.Syntax.Literal
 import ExSql.Syntax.Logical
 
 newtype Printer (g :: * -> *) (v :: (* -> *) -> * -> *) = Printer { runPrinter :: forall a. Maybe Relativity -> Maybe Relativity -> v g a -> (Text, DList PersistValue) }
@@ -35,6 +36,12 @@ prettyBinOp p l c r op a b =
     let (lt, lps) = p l (Just c) a
         (rt, rps) = p (Just c) r b
     in (handleBracket l c r $ lt `mappend` op `mappend` rt, lps `mappend` rps)
+
+prettyFun :: Text -> [(Text, DList PersistValue)] -> (Text, DList PersistValue)
+prettyFun fname args = (fname `mappend` "(" `mappend` Text.intercalate ", " xs `mappend` ")", ps)
+    where
+    xs = map fst args
+    ps = mconcat $ map snd args
 
 prettyLiteral :: Maybe Relativity -> Maybe Relativity -> Literal g a -> (Text, DList PersistValue)
 prettyLiteral _ _ (LitInt a) = ("?", return $ PersistInt64 a)
@@ -86,6 +93,21 @@ prettyLogical p l r (Conjunction a0 a1) =
 prettyLogical p l r (Disjunction a0 a1) =
     let c = Relativity (Precedence 16) LeftToRight
     in prettyBinOp p l c r " OR " a0 a1
+
+prettyFunction :: (forall b. Maybe Relativity -> Maybe Relativity -> Expr xs Identity b -> (Text, DList PersistValue)) -> Maybe Relativity -> Maybe Relativity -> Function (Expr xs Identity) a -> (Text, DList PersistValue)
+prettyFunction _ _ _ (Function0 fname) = prettyFun fname []
+prettyFunction p _ _ (Function1 fname a0) =
+    prettyFun fname $ [p Nothing Nothing a0]
+prettyFunction p _ _ (Function2 fname a0 a1) =
+    prettyFun fname $ [p Nothing Nothing a0, p Nothing Nothing a1]
+prettyFunction p _ _ (Function3 fname a0 a1 a2) =
+    prettyFun fname $ [p Nothing Nothing a0, p Nothing Nothing a1, p Nothing Nothing a2]
+prettyFunction p _ _ (Function4 fname a0 a1 a2 a3) =
+    prettyFun fname $ [p Nothing Nothing a0, p Nothing Nothing a1, p Nothing Nothing a2, p Nothing Nothing a3]
+prettyFunction p _ _ (Function5 fname a0 a1 a2 a3 a4) =
+    prettyFun fname $ [p Nothing Nothing a0, p Nothing Nothing a1, p Nothing Nothing a2, p Nothing Nothing a3, p Nothing Nothing a4]
+prettyFunction p _ _ (Function6 fname a0 a1 a2 a3 a4 a5) =
+    prettyFun fname $ [p Nothing Nothing a0, p Nothing Nothing a1, p Nothing Nothing a2, p Nothing Nothing a3, p Nothing Nothing a4, p Nothing Nothing a5]
 
 handleBracket :: Maybe Relativity -> Relativity -> Maybe Relativity -> Text -> Text
 handleBracket l c r s = if needBracket l c r
---
+++ src/ExSql/Syntax/Function.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE GADTs, RankNTypes, FlexibleContexts #-}
+module ExSql.Syntax.Function
+    ( Function(..)
+    ) where
+
+import ExSql.Syntax.Class
+
+import Data.Text (Text)
+
+data Function g x where
+    Function0 :: Text -> Function g x
+    Function1 :: Text -> g a0 -> Function g x
+    Function2 :: Text -> g a0 -> g a1 -> Function g x
+    Function3 :: Text -> g a0 -> g a1 -> g a2 -> Function g x
+    Function4 :: Text -> g a0 -> g a1 -> g a2 -> g a3 -> Function g x
+    Function5 :: Text -> g a0 -> g a1 -> g a2 -> g a3 -> g a4 -> Function g x
+    Function6 :: Text -> g a0 -> g a1 -> g a2 -> g a3 -> g a4 -> g a5 -> Function g x
+
+instance Hoist Function where
+    hoist _ (Function0 n) = Function0 n
+    hoist f (Function1 n a) = Function1 n (f a)
+    hoist f (Function2 n a0 a1) = Function2 n (f a0) (f a1)
+    hoist f (Function3 n a0 a1 a2) = Function3 n (f a0) (f a1) (f a2)
+    hoist f (Function4 n a0 a1 a2 a3) = Function4 n (f a0) (f a1) (f a2) (f a3)
+    hoist f (Function5 n a0 a1 a2 a3 a4) = Function5 n (f a0) (f a1) (f a2) (f a3) (f a4)
+    hoist f (Function6 n a0 a1 a2 a3 a4 a5) = Function6 n (f a0) (f a1) (f a2) (f a3) (f a4) (f a5)
