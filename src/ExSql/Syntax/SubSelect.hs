d03b9414d9657b496c3eb6e7b7ac7ddf97a3738e
tkaaad97 <tkaaad97@gmail.com>
2018-05-12 19:37:36 +0900

    sub select

---
+++ src/ExSql/Syntax/SubSelect.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE KindSignatures, GADTs, FlexibleContexts #-}
+module ExSql.Syntax.SubSelect
+    ( SubSelect(..)
+    , subSelect
+    , subSelectValues
+    ) where
+
+import Data.Extensible (Member)
+import Database.Persist.Sql (Single)
+import ExSql.Syntax.Class
+import ExSql.Syntax.Internal.Types
+import ExSql.Syntax.SelectQuery
+
+data SubSelect (g :: * -> *) a where
+    SubSelect :: SelectQuery g (Single a) -> SubSelect g a
+    SubSelectValues :: SelectQuery g a -> SubSelect g (ValueList a)
+
+instance Hoist SubSelect where
+    hoist f (SubSelect query) = SubSelect (hoist f query)
+    hoist f (SubSelectValues query) = SubSelectValues (hoist f query)
+
+subSelect :: (Ast g, Monad m, Member (NodeTypes g) SubSelect)
+    => SelectQuery (g m) (Single a) -> g m a
+subSelect = mkAst . return . SubSelect
+
+subSelectValues :: (Ast g, Monad m, Member (NodeTypes g) SubSelect)
+    => SelectQuery (g m) a -> g m (ValueList a)
+subSelectValues = mkAst . return . SubSelectValues
