457d1b10bc5961b434461ba024c6108e38abc207
tkaaad97 <tkaaad97@gmail.com>
2018-07-01 00:13:30 +0900

    add file

---
+++ src/ExSql/Syntax/Internal/SelectQueryStage.hs
@@ -0,0 +1,15 @@
+module ExSql.Syntax.Internal.SelectQueryStage
+    ( Neutral
+    , FieldsSpecified
+    , Aggregated
+    , AggFieldsSpecified
+    ) where
+
+
+data Neutral
+
+data FieldsSpecified
+
+data Aggregated
+
+data AggFieldsSpecified
